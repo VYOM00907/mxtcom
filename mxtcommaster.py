a803672641cf7c5719be19616490a8268a0f8f14
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-23 21:06:59 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -3,7 +3,7 @@ import socket as s
 print("communication master module for mxt")
 hn = "https://www.geeksforgeeks.org/python-program-find-ip-address/"
 ip = s.gethostbyname('checkip.dyndns.com')
-s.conn(ip)
+s.connect((ip, 443))
 
 
 
