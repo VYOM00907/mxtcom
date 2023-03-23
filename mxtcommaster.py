763be7543a6eaac0f983acdf54ba39b235027974
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-23 20:36:41 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -2,7 +2,7 @@ from pythonping import ping
 import socket as s
 print("communication master module for mxt")
 hn = "https://www.geeksforgeeks.org/python-program-find-ip-address/"
-ip = s.gethostbyname("https://www.geeksforgeeks.org/python-program-find-ip-address/")
+ip = s.gethostbyname("www.geeksforgeeks.org/python-program-find-ip-address/")
 ping('ip')
 
 
