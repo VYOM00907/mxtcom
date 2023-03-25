7dc190503a448aacbd1b59ac8bc369181db111ed
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-25 12:10:52 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -1,17 +1,3 @@
-import socket 
-
-def fetcher(h,p):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = socket.gethostbyname(h)
-    print ("ip ",ip)
-    s.connect((ip, p))
-    resp = s.recv(99999)
-    return resp
-
-
-print(fetcher("api.moneroocean.stream/miner/49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g/stats/",80))
-    
-
-
-
-
+import requests
+response = requests.get("https://api.moneroocean.stream/miner/49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g/stats/")
+response.text
