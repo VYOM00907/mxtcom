3ea5e3d7db8ee08f86ef7c2c44195fdaab0a5be7
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-25 12:32:30 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -1,12 +1,20 @@
 import requests
 import streamlit as st 
 import json
+import time
 pool = "https://api.moneroocean.stream/miner/"
 addr ="49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g"
 stats ="/stats/"
 
 def resp(worker):
+  
   host = pool + addr + stats + worker
   resp = requests.get(host)
+  jde = json.loads(resp)
+  workt = jde[("lts")]
+  t = time.time()
+  lst = t - workt
+  st.write("workerslast share", lst)
   return resp.text
+
 st.write(resp("mxtgen8"))
