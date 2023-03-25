59e3f40409b539e96ca10282dfa4b0986a4aad29
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-25 12:23:03 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -5,7 +5,8 @@ pool = "https://api.moneroocean.stream/miner/"
 addr ="49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g"
 stats ="/stats/"
 
-def resp(host):
+def resp(worker):
+  host = pool + addr + stats + worker
   resp = requests.get(host)
   return resp.text
-st.write(resp(pool + addr + stats + "mxt"))
+st.write(resp("mxtgen8"))
