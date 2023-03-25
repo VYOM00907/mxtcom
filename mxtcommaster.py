a4bb6f921afb069b5d5a6b8260968918f4481725
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-25 12:20:15 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -1,6 +1,11 @@
 import requests
 import streamlit as st 
+import json
+pool = "https://api.moneroocean.stream/miner/"
+addr ="49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g"
+stats ="/stats/"
+
 def resp(host):
   resp = requests.get(host)
   return resp.text
-st.write(resp("https://api.moneroocean.stream/miner/49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g/stats/"))
+st.write(resp(pool + addr + stats + "mxt"))
