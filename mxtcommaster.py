561f34ea6fc968fba0d87e906a428a59076aee1e
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-25 11:53:54 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -3,7 +3,7 @@ import socket
 def fetcher(h,p):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ip = socket.gethostbyname(h)
-    print ip
+    print ("ip ",ip)
     s.connect((ip, p))
     resp = s.recv()
     return resp
