1b642fdd4b8eda9acc817a64813bced44f4e9c0e
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-27 20:33:54 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -18,6 +18,53 @@ def resp(worker):
   t = time.time()
   lst = t - workt
   lstnt = lst / 60
+  if lstnt >= 20:
+    # mxt v 1
+    if worker == "mxt":
+      requests.get("https://vyom00907-mxtcot-app-ikd0lj.streamlit.app/")
+    if worker == "mxtgen01":
+      requests.get("https://mxt-vyom00907.streamlit.app/")
+    if worker == "mxtgen1":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen1-pck5yt.streamlit.app/")
+    if worker == "mxtgen2":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen2-w6l8nj.streamlit.app/")
+    if worker == "mxtgen3":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen3-e202e2.streamlit.app/")
+    if worker == "mxtgen4":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen4-pxwkrs.streamlit.app/")
+    if worker == "mxtgen5":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen5-yjt88z.streamlit.app/")
+    if worker == "mxtgen6":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen6-yto63j.streamlit.app/")
+    if worker == "mxtgen7":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen7-jhx3x5.streamlit.app/")
+    if worker == "mxtgen8":
+      requests.get("https://vyom00907-mxtfaujh-mxtgen8-klnuoc.streamlit.app/")
+    #mxt v2 
+    if worker == "mv2gen1":
+      requests.get("https://vyom00907-mv2faujh-mv2gen1-liilfe.streamlit.app/")
+    if worker == "mv2gen2":
+      requests.get("https://vyom00907-mv2faujh-mv2gen2-k8o2nr.streamlit.app/")
+    if worker == "mv2gen3":
+      requests.get("https://vyom00907-mv2faujh-mv2gen3-zezt1k.streamlit.app/")
+    if worker == "mv2gen4":
+      requests.get("https://vyom00907-mv2faujh-mv2gen4-ct891n.streamlit.app/")
+    if worker == "mv2gen5":
+      requests.get("https://vyom00907-mv2faujh-mv2gen5-mnagn0.streamlit.app/")
+    if worker == "mv2gen6":
+      requests.get("https://vyom00907-mv2faujh-mv2gen6-u9yndz.streamlit.app/")
+    if worker == "mv2gen7":
+      requests.get("https://vyom00907-mv2faujh-mv2gen7-u8kxke.streamlit.app/")
+    if worker == "mv2gen8":
+      requests.get("https://vyom00907-mv2faujh-mv2gen8-irrnfx.streamlit.app/")
+    if worker == "mv2gen9":
+      requests.get("https://vyom00907-mv2faujh-mv2gen9-rlq22f.streamlit.app/")
+    if worker == "mv2gen10":
+      requests.get("https://vyom00907-mv2faujh-mv2gen10-sz9de1.streamlit.app/")
+      
+      
+      
+      
 
   st.write(worker,"last share", lstnt, "mins ago")
   
@@ -27,7 +74,7 @@ dcon = st.container()
 
 while 1==1 :
   dcon.write(resp("mxt"))
-  dcon.write(resp("stream"))
+  dcon.write(resp("mxtgen01"))
   dcon.write(resp("mxtgen1"))
   dcon.write(resp("mxtgen2"))
   dcon.write(resp("mxtgen3"))
