2c48bfb4fb5d92b1265feb91daccb46ac4ad6275
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-27 10:08:05 +0530

    Update mxtcommaster.py

--- mxtcommaster.py
+++ mxtcommaster.py
@@ -7,19 +7,44 @@ pool = "https://api.moneroocean.stream/miner/"
 addr ="49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g"
 stats ="/stats/"
 
+
 def resp(worker):
   
   host = pool + addr + stats + worker
   res = requests.get(host)
-  time.sleep(15)
+  time.sleep(10)
   jde = json.loads(str(res.text))
   workt = jde[("lts")]
   t = time.time()
   lst = t - workt
   lstnt = lst / 60
-  st.write("workerslast share", lstnt, "mins ago")
+  st.write(worker,"workerslast share", lstnt, "mins ago")
   
-  return res.text
+
+dcon = st.container()
+
+
 while 1==1 :
-  st.snow()
-  st.write(resp("mxt"))
+  dcon.write(resp("mxt"))
+  dcon.write(resp("stream"))
+  dcon.write(resp("mxtgen1"))
+  dcon.write(resp("mxtgen2"))
+  dcon.write(resp("mxtgen3"))
+  dcon.write(resp("mxtgen4"))
+  dcon.write(resp("mxtgen5"))
+  dcon.write(resp("mxtgen6"))
+  dcon.write(resp("mxtgen8"))
+  dcon.write(resp("mv2gen1"))
+  dcon.write(resp("mv2gen2"))
+  dcon.write(resp("mv2gen3"))
+  dcon.write(resp("mv2gen4"))
+  dcon.write(resp("mv2gen5"))
+  dcon.write(resp("mv2gen6"))
+  dcon.write(resp("mv2gen7"))
+  dcon.write(resp("mv2gen8"))
+  dcon.write(resp("mv2gen9"))
+  dcon.write(resp("mv2gen10"))
+  
+
+
+  
