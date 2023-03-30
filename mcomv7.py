636decd12f0d8cb25e5b08b6627129da7a278a29
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-30 16:17:20 +0530

    Update mcomv7.py

--- mcomv7.py
+++ mcomv7.py
@@ -19,7 +19,7 @@ def resp(worker):
   t = time.time()
   lst = t - workt
   lstnt = lst / 60
-  if lstnt >= 30:
+  if lstnt >= 20:
     #mv4
     if worker == "mv7gen1":
       requests.get("https://vyom00907-mv7-mv7gen1-txg47k.streamlit.app/")
